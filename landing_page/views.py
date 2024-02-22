b45cd899c0773971b3cf0600a56cd849765e62b6
sangeeta-g-d <sangeetadani599@gmail.com>
2024-02-22 12:47:19 +0530

    first commit

---
+++ landing_page/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect,HttpResponseForbidden,HttpResponseBadRequest
+from django.template import loader
+from django.contrib.auth import authenticate,login,logout
+# Create your views here.
+
+
+def index(request):
+    return render(request,'index.html')
